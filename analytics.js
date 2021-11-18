3beb383b21fbab292d6a6cc07c75111c64a59879
yolanbottema <59021664+yolanbottema@users.noreply.github.com>
2021-11-18 13:58:56 +0100

    google analytics

--- analytics.js
+++ analytics.js
@@ -1,5 +1,7 @@
-window.dataLayer = window.dataLayer || [];
-function gtag() { dataLayer.push(arguments); }
-gtag('js', new Date());
 
-gtag('config', 'G-PLZWGP59EE');
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-160639599-1');
+
